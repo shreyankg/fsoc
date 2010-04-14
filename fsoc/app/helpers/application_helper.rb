375eea0f8dbb96ad4aee343bce2b0d24211385bf
Shreyank Gupta <sgupta@redhat.com>
2010-04-14 19:07:16 +0530

    Using single layout + Userbar improved

--- fsoc/app/helpers/application_helper.rb
+++ fsoc/app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def highlighted
+    return 'class="current_page_item"'
+  end
+  
 end
